ea4917ba68431c3f769d56db8dc923e39fc67bdb
KyuHyeonYang <156461703+KyuHyeonYang@users.noreply.github.com>
2024-05-06 23:26:42 +0900

    Delete ch08_interface directory

--- ch08_interface/sec04/ISearchable.java
+++
@@ -1,6 +0,0 @@
-package ch08_interface.sec04;
-
-public interface ISearchable {
-//추상메소드 선언
-	void search(String url);
-}
--- ch08_interface/sec05/InterFaceA.java
+++
@@ -1,6 +0,0 @@
-package ch08_interface.sec05;
-
-public interface InterFaceA {
-//추상메소드
-	public void methodA();
-}
